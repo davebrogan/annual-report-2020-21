8e4bc4aa483945fb85640b82fac9da0d47b0c161
davebrogan <davebrogan@users.noreply.github.com>
2021-09-17 11:41:59 -0600

    everything from notopnav ported to regular prod files and tested in site manager. looks good. going to merge with the other dev branch

--- code/js/annual-report-2021.js
+++ code/js/annual-report-2021.js
@@ -7,7 +7,6 @@ let body = document.body;
 let knockoutPosTop;
 let verticalBgSlider;
 const origPos = knockout.getBoundingClientRect().top;
-
 let vh = window.innerHeight;
 let vw = window.innerWidth;
 
@@ -19,11 +18,7 @@ let vpAspect = function () {
     let viewportHeight = window.innerHeight;
     let viewportWidth = window.innerWidth;
     let aspect = (viewportHeight / viewportWidth);
-    if (aspect < 0.68) {
-        return true;
-    } else {
-        return false;
-    }
+    return aspect < 0.68;
 };
 
 window.addEventListener('load', function(e) {
@@ -33,19 +28,19 @@ window.addEventListener('load', function(e) {
         //then slides bg image down to meet cutout if position -px
         window.addEventListener('scroll', function(e) {
             knockoutPosTop = knockout.getBoundingClientRect().top; //distance knockout image from window top
-            //once the knockout crosses threshhold the distance from top is used to pull the bg image down to meet the knockout
+            //once the knockout crosses threshold the distance from top is used to pull the bg image down to meet the knockout
             if (knockoutPosTop <= bgOffsetY && knockoutPosTop > 0) {
                 //within the conditional range the distance from knockout to top is used
                 verticalBgSlider = knockoutPosTop;
                 //the make up value is divided by the knockout position to add scaled values to smoothly get to the make up value position 0 + bgOffsetMakeUp
                 body.style.backgroundPositionY = -(verticalBgSlider) + (bgOffsetMakeUp/verticalBgSlider) + 'px';
-            } else if (window.pageYOffset > vh || knockoutPosTop == 0) {
+            } else if (window.pageYOffset > vh || knockoutPosTop === 0) {
                 //this freezes the bg image movement if the knockout has reached the top
                 body.style.backgroundPositionY = bgOffsetMakeUp + 'px';
             }
         })
     } else {
-        body.style.backgroundPositionY = 0;
+        bannerWrap.style.backgroundPositionY = '0';
     }
 })
 
@@ -57,16 +52,19 @@ window.addEventListener('resize', function(e) {
             if (knockoutPosTop <= bgOffsetY && knockoutPosTop > 0) {
                 verticalBgSlider = knockoutPosTop;
                 body.style.backgroundPositionY = -(verticalBgSlider) + (bgOffsetMakeUp/verticalBgSlider) + 'px';
-            } else if (window.pageYOffset > vh || knockoutPosTop == 0) {
+            } else if (window.pageYOffset > vh || knockoutPosTop === 0) {
                 body.style.backgroundPositionY = bgOffsetMakeUp + 'px';
             }
         })
     } else {
-        body.style.backgroundPositionY = 0;
+        body.style.backgroundPositionY = '0';
     }
+    animateFirstTitle();
 })
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', animateFirstTitle);
+
+function animateFirstTitle(event) {
     knockoutPosTop = knockout.getBoundingClientRect().top; //distance knockout image from window top
     //show/hide first-title
     if (knockoutPosTop <= 0) {
@@ -77,19 +75,5 @@ window.addEventListener('scroll', function() {
     } else if (knockoutPosTop >= origPos) {
         firstTitle.classList.remove('animate-up');
     }
-})
-
-//hide div until clarendon loads
-//this method of loading custom fonts was tested against others including some native browser behavior, css inline code, etc, with inconclusive results.
-//this method relies on traditional @font-face techniques for implementing custom fonts. See the custom-type-starter.scss file.
-
-document.fonts.ready.then(function () {
-    var titleContent = document.getElementById('hide-title-load');
-    titleContent.style.visibility = "visible";
-});
-
-document.fonts.ready.then(function() {
-    var statContent = document.getElementById('hide-clar-load');
-    statContent.style.visibility = "visible";
-});
+}
 
--- code/js/ar-home-nonav.js
+++ code/js/ar-home-nonav.js
@@ -3,13 +3,10 @@
 let firstTitle = document.getElementById('first-title');
 let knockout = document.getElementById('knockout');
 let body = document.body;
-let pageBody = document.getElementsByClassName('showcase-html-section')[0];
-let bannerWrap = document.getElementsByClassName('banner-wrap')[0];
 
 let knockoutPosTop;
 let verticalBgSlider;
 const origPos = knockout.getBoundingClientRect().top;
-console.log('original position is ' + origPos);
 let vh = window.innerHeight;
 let vw = window.innerWidth;
 
@@ -26,14 +23,11 @@ let vpAspect = function () {
 
 window.addEventListener('load', function(e) {
     //sets bg image position based on aspect ratio to eliminate whitespace issue created by -px positioning
-    console.log('knockout pos is ' + knockoutPosTop);
     if (vpAspect()) {
         body.style.backgroundPositionY = -bgOffsetY + 'px';
-        console.log('aspect is true');
         //then slides bg image down to meet cutout if position -px
         window.addEventListener('scroll', function(e) {
             knockoutPosTop = knockout.getBoundingClientRect().top; //distance knockout image from window top
-            console.log('knockout pos is ' + knockoutPosTop);
             //once the knockout crosses threshold the distance from top is used to pull the bg image down to meet the knockout
             if (knockoutPosTop <= bgOffsetY && knockoutPosTop > 0) {
                 //within the conditional range the distance from knockout to top is used
@@ -65,6 +59,12 @@ window.addEventListener('resize', function(e) {
     } else {
         body.style.backgroundPositionY = '0';
     }
+    animateFirstTitle();
+})
+
+window.addEventListener('scroll', animateFirstTitle);
+
+function animateFirstTitle(event) {
     knockoutPosTop = knockout.getBoundingClientRect().top; //distance knockout image from window top
     //show/hide first-title
     if (knockoutPosTop <= 0) {
@@ -75,33 +75,5 @@ window.addEventListener('resize', function(e) {
     } else if (knockoutPosTop >= origPos) {
         firstTitle.classList.remove('animate-up');
     }
-})
-
-window.addEventListener('scroll', function() {
-    knockoutPosTop = knockout.getBoundingClientRect().top; //distance knockout image from window top
-    //show/hide first-title
-    if (knockoutPosTop <= 0) {
-        firstTitle.classList.add('animate-up');
-        knockout.classList.add('freeze-knockout');
-    } else if (knockoutPosTop > 0 && knockout.classList.contains('freeze-knockout')) {
-        knockout.classList.remove('freeze-knockout');
-    } else if (knockoutPosTop >= origPos) {
-        firstTitle.classList.remove('animate-up');
-    }
-})
-
-//hide div until clarendon loads
-//this method of loading custom fonts was tested against others including some native browser behavior, css inline code, etc, with inconclusive results.
-//this method relies on traditional @font-face techniques for implementing custom fonts. See the custom-type-starter.scss file.
-
-/*document.fonts.ready.then(function () {
-    var titleContent = document.getElementById('hide-title-load');
-    titleContent.style.visibility = "visible";
-});
-
-document.fonts.ready.then(function() {
-    var statContent = document.getElementById('hide-clar-load');
-    console.log(statContent);
-    statContent.style.visibility = "visible";
-});*/
+}
 
